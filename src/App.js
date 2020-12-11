5ec6ad4472dfa49a4d35346d6aeeb1046ec81a76
ChristopherZalapa <chriszalapa@gmail.com>
2020-12-10 18:25:44 -0800

    Scroll to top deleted

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import AboutPage from './pages/AboutPage'
 import MyProjects from './pages/MyProjects'
 import MyStack from './pages/MyStack'
 import ContactPage from './pages/ContactPage'
-import ScrollToTop from './components/ScrollToTop'
 
 import './App.scss';
 
@@ -14,7 +13,6 @@ function App () {
   return (
     <Router>
       <Navbar />
-      <ScrollToTop />
       <Switch>
         <Route exact path="/" component={ HomePage } />
         <Route path="/about" component={ AboutPage } />
