91ddcfdb7073f0b2d14275c8dc49653ba038a5a4
ChristopherZalapa <chriszalapa@gmail.com>
2021-01-14 20:31:54 -0800

    About Page H1 P & Img Basic Style

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -8,9 +8,7 @@ class AboutPage extends Component {
                     <h1>About Me</h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error quae quos unde numquam id possimus? Officiis modi esse alias ipsam sequi repellat cumque, quidem repellendus suscipit, doloremque, provident inventore nostrum!</p>
                 </div>
-                <div className="pic-container">
-                    <img src="/images/9TksEPPg-removebg-preview.png" alt="man"/>
-                </div>
+                <img src="/images/9TksEPPg-removebg-preview.png" alt="man"/>
             </div>
         )
     }
